3646bba229f0ce0b43e813b3dc3064bbd239b2eb
hungvu <hungvu19061991@gmail.com>
2024-10-20 14:25:34 +0700

    git init main

---
+++ src/main/java/org/example/bytequanta/BytequantaApplication.java
@@ -0,0 +1,13 @@
+package org.example.bytequanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytequantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytequantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bytequanta/BytequantaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bytequanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytequantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
